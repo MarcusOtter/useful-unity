316af2884321d3ce6b1a57dcbf094239ab254e52
MarcusOtter <github@otterstrom.dev>
2022-07-14 20:57:58 +0200

    Add CancellationToken to FireAndForget

--- src/CoroutineHelpers.cs
+++ src/CoroutineHelpers.cs
@@ -1,18 +1,20 @@
 using System;
 using System.Collections;
+using System.Threading;
 using UnityEngine;
 
-public static class CoroutineHelpers
+public static class MonoBehaviourHelpers
 {
-	public static void FireAndForgetWithDelay(this MonoBehaviour monoBehaviour, float seconds, Action action)
+	public static void FireAndForgetWithDelay(this MonoBehaviour monoBehaviour, float seconds, Action action, CancellationToken cancellationToken = default)
 	{
-		monoBehaviour.StartCoroutine(InvokeActionAfterDelay(seconds, action));
+		monoBehaviour.StartCoroutine(InvokeActionAfterDelay(seconds, action, cancellationToken));
 	}
 
 	// ReSharper disable Unity.PerformanceAnalysis
-	private static IEnumerator InvokeActionAfterDelay(float seconds, Action action)
+	private static IEnumerator InvokeActionAfterDelay(float seconds, Action action, CancellationToken cancellationToken)
 	{
 		yield return new WaitForSeconds(seconds);
+		if (cancellationToken.IsCancellationRequested) yield break;
 		action();
 	}
 }
